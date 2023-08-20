2a9ed0f45a34ae8ec13ec27f4d3d22c401f9db2e
emilyjevans <emilyjevans94@hotmail.com>
2023-08-20 13:13:04 +0100

    adding button and responsiveness

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import { useState, useEffect } from "react";
 import SearchBar from "./components/SearchBar/SearchBar";
 import { Octokit } from "octokit";
 import Menu from "./components/Menu/Menu";
-import { Grid } from "@mui/material";
+import { Grid, Button } from "@mui/material";
 
 function App() {
-  const [user, setUser] = useState("emilyjevans");
-  const [menuData, setMenuData] = useState([{ name: "Choose a repo" }]);
-  const [repo, setRepo] = useState("nc-giggle");
-  const [commits, setCommits] = useState([{ commit: { message: "blank" } }]);
+  const [user, setUser] = useState("bbc");
+  const [menuData, setMenuData] = useState([]);
+  const [repo, setRepo] = useState(".github");
+  const [commits, setCommits] = useState([
+    {
+      commit: { message: "blank", author: { date: "2021-12-16T10:05:52Z" } },
+      author: { login: "Author" },
+    },
+  ]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -28,7 +33,6 @@ function App() {
         "X-GitHub-Api-Version": "2022-11-28",
       },
     });
-    console.log(response.data);
     return response.data;
   }
 
@@ -58,29 +62,46 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Find out the latest git commit!</h1>
-        <Grid container spacing={2}>
-          <Grid item xs={5}>
-            <p>Enter a github user and hit enter</p>
+        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 1, sm: 2 }}>
+          <Grid item xs={1} sm={1}>
+            <p>Enter a github user</p>
             <form onSubmit={handleSubmit}>
-              <SearchBar user={user} setUser={setUser} />
+              <SearchBar user={user} setUser={setUser} sx={{ height: 50 }} />{" "}
+              <Button variant="contained" type="submit" sx={{ height: 55 }}>
+                Submit
+              </Button>
             </form>
-            <p>And choose a repo</p>
-            <Menu
-              data={menuData}
-              repo={repo}
-              user={user}
-              setRepo={setRepo}
-              setCommits={setCommits}
-            />
+            {menuData.length > 0 && (
+              <>
+                <p>And choose a repo</p>
+                <Menu
+                  data={menuData}
+                  repo={repo}
+                  user={user}
+                  setRepo={setRepo}
+                  setCommits={setCommits}
+                />
+              </>
+            )}
           </Grid>
-          <Grid item xs={5}>
-            <p>
-              Current Repo: <b>{repo}</b>
-            </p>
-            <p>
-              Latest commit is <br />
-              <i>"{commits[0].commit.message}"</i>
-            </p>
+          <Grid item xs={1} sm={1}>
+            {repo !== "nc-giggle" && (
+              <>
+                <p>
+                  Current Repo: <b>{repo}</b>
+                </p>
+                <p>
+                  Latest commit is <br />
+                  <i>"{commits[0].commit.message}"</i>
+                </p>
+                <p>
+                  Author: <b>{commits[0].author.login}</b>
+                </p>
+                <p>
+                  Date: <b>{commits[0].commit.author.date}</b>
+                </p>
+              </>
+            )}
           </Grid>
         </Grid>
       </header>
